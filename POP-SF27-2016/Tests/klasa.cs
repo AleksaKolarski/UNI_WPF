e3982a4125d479b6aada1e5e2639040832f21b17
student <student@ftn.ns>
2017-10-16 12:12:12 +0200

    Initial commit

---
+++ POP-SF27-2016/Tests/klasa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstorImena
+{
+    class klasa
+    {
+        private string naziv;
+
+        public string GetNaziv()
+        {
+            return this.naziv;
+        }
+        public void SetNaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+
+        //public string Ime;
+        public string Ime { get; set; }
+    }
+}
---
+++ POP-SF27-2016/Tests/test1.cs
@@ -0,0 +1,25 @@
+﻿using ProstorImena;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_SF27_2016.Tests
+{
+    class test1
+    {
+        public test1()
+        {
+            klasa a = new klasa();
+            a.SetNaziv("Naziv svih naziva");
+            Console.WriteLine("Naziv je : " + a.GetNaziv());
+
+            a.Ime = "ime svih imena";
+            Console.WriteLine("Ime je: " + a.Ime);
+
+
+            Console.ReadLine();
+        }
+    }
+}
