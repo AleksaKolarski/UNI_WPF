872a9c76d0dda6deafa85d38a8a606cabf0c4751
Aleksa-Laptop <aleksa.kolarski@yahoo.com>
2017-11-06 10:40:42 +0100

    Dodat WPF projekat

---
+++ POP-SF27-2016-WPF/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace POP_SF27_2016_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
