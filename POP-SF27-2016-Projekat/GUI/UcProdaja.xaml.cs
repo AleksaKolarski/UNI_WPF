deb0bed8b38819cc801bf0324b7337b6bb8d1834
LetecaPegla <>
2017-12-28 13:56:30 +0100

    Pretraga prodaje gotova

--- POP-SF27-2016-Projekat/GUI/UcProdaja.xaml.cs
+++ POP-SF27-2016-Projekat/GUI/UcProdaja.xaml.cs
@@ -24,13 +24,34 @@ namespace POP_SF27_2016_Projekat.GUI
 
         public UcProdaja()
         {
+            view = CollectionViewSource.GetDefaultView(ProdajaNamestaja.prodajaNamestajaCollection);
             InitializeComponent();
 
-            view = CollectionViewSource.GetDefaultView(ProdajaNamestaja.prodajaNamestajaCollection);
+            view.Filter = Filter;
             dgProdaja.ItemsSource = view;
 
             btnAdd.DataContext = Korisnik.Trenutni.TipKorisnika.Dozvole;
         }
+
+        #region Filters
+        private bool Filter(object obj)
+        {
+            var text = ((ComboBoxItem)cbPretraga.SelectedItem).Content.ToString();
+            if (text.Equals("Id"))
+            {
+                return ((ProdajaNamestaja)obj).Id.ToString().IndexOf(tbPretraga.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+            }
+            else if (text.Equals("Kupac"))
+            {
+                return ((ProdajaNamestaja)obj).Kupac.IndexOf(tbPretraga.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+            }
+            else if (text.Equals("Broj racuna"))
+            {
+                return ((ProdajaNamestaja)obj).BrojRacuna.IndexOf(tbPretraga.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+            }
+            return false;
+        }
+        #endregion
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
@@ -47,6 +68,16 @@ namespace POP_SF27_2016_Projekat.GUI
                 tbPDV.DataContext = (ProdajaNamestaja)dgProdaja.SelectedItem;
                 tbUkupnaCena.DataContext = (ProdajaNamestaja)dgProdaja.SelectedItem;
             }
+        }
+
+        private void cbPretraga_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            view.Refresh();
+        }
+
+        private void tbPretraga_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            view.Refresh();
         }
     }
 }
